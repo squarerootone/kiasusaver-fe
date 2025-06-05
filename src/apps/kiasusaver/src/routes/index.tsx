da65bbd82f4466f6c305bda83406943cf5588112
THL <hailong.tran@squareroot.one>
2025-06-05 06:47:50 +0000

    (feat)  basic qwik app set up

---
+++ src/apps/kiasusaver/src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <h1>Hi 👋</h1>
+      <div>
+        Can't wait to see what you build with qwik!
+        <br />
+        Happy coding.
+      </div>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "kiasusaver",
+      content: "Kiasu Saver is a web application designed to help users find the best bonus saving accounts for their saving.",
+    },
+  ],
+};
